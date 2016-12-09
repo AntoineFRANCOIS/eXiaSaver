e3a4073f041563a5b1c6774d244527da73c7ce91
AntoineFRANCOIS <antoine.francois@viacesi.fr>
2016-12-09 16:24:23 +0100

    Delete fonctions.h

--- fonctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef	FONCTIONS_H_
-#define	FONCTIONS_H_
-
-void	changemementCarre(int *tab, int hauteur, int largeur);
-void	affichage(int *tab, int hauteur, int largeur);
-int	aleatoire();
-void	stat();
-
-#endif	/*FONCTIONS_H_*/
